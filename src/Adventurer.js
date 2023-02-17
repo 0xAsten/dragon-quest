0c6cab9921748e66103fa220ce01541f30e93239
Asten <0xasten@gmail.com>
2023-02-17 23:02:49 +0800

    eslint format

--- src/Adventurer.js
+++ src/Adventurer.js
@@ -1,70 +1,68 @@
-import React, { useState, useContext, Fragment, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { Contract, uint256, number } from 'starknet';
+import React, { useState, useContext, Fragment, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { Contract, uint256, number } from 'starknet'
 
-import { UserContext } from './contexts/user.context';
-import adventurerAbi from './abis/adventurer_abi.json';
+import { UserContext } from './contexts/user.context'
+import adventurerAbi from './abis/adventurer_abi.json'
 
-import './App.css';
+import './App.css'
 
 function Adventurer() {
-  const { currentUser } = useContext(UserContext);
+  const { currentUser } = useContext(UserContext)
 
-  const [count, setCount] = useState(-1);
-  const [adventurers, setAdventurers] = useState([]);
-  const [isMinting, setIsMinting] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
+  const [count, setCount] = useState(-1)
+  const [adventurers, setAdventurers] = useState([])
+  const [isMinting, setIsMinting] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
 
   const contractAddress =
-    '0x064c4c4e497c52b046c09f7565e61d2d2a5da9b020bc8c9fd50aff0371b7a6b0';
+    '0x064c4c4e497c52b046c09f7565e61d2d2a5da9b020bc8c9fd50aff0371b7a6b0'
 
   const getAdventurers = async () => {
     try {
-      setIsLoading(true);
+      setIsLoading(true)
       const contract = new Contract(
         adventurerAbi,
         contractAddress,
         currentUser.provider
-      );
-      const { balance: balance } = await contract.balanceOf(
-        currentUser.address
-      );
+      )
+      const { balance } = await contract.balanceOf(currentUser.address)
 
-      const balanceOf = parseInt(number.toFelt(uint256.uint256ToBN(balance)));
+      const balanceOf = parseInt(number.toFelt(uint256.uint256ToBN(balance)))
 
-      setCount(balanceOf);
+      setCount(balanceOf)
 
-      const results = [];
+      const results = []
       for (let i = 0; i < balanceOf; i++) {
-        const result = await getTokenByIndex(contract, i);
-        results.push(result);
-        setAdventurers([...results]);
+        const result = await getTokenByIndex(contract, i)
+        results.push(result)
+        setAdventurers([...results])
       }
     } catch (error) {
-      alert(error.message);
+      alert(error.message)
     }
 
-    setIsLoading(false);
-  };
+    setIsLoading(false)
+  }
 
   const getTokenByIndex = async (contract, i) => {
-    const { tokenId: tokenId } = await contract.tokenOfOwnerByIndex(
+    const { tokenId } = await contract.tokenOfOwnerByIndex(
       currentUser.address,
       uint256.bnToUint256(number.toBN(i))
-    );
+    )
 
-    const adventurerId = parseInt(number.toFelt(uint256.uint256ToBN(tokenId)));
+    const adventurerId = parseInt(number.toFelt(uint256.uint256ToBN(tokenId)))
 
     return (
       <div key={adventurerId} className='adventurer'>
         <Link to={'/quest/' + adventurerId}>
           {/* <img src={process.env.PUBLIC_URL + "adventurer.png"} /> */}
-          <img src={'/adventurer.png'} />
+          <img alt='' src={'/adventurer.png'} />
         </Link>
         <p># {adventurerId}</p>
       </div>
-    );
-  };
+    )
+  }
 
   const mintAdventurer = async () => {
     try {
@@ -72,34 +70,35 @@ function Adventurer() {
         adventurerAbi,
         contractAddress,
         currentUser.provider
-      );
+      )
 
-      setIsMinting(true);
+      setIsMinting(true)
       const { transaction_hash: txHash } = await contract.mint(
         currentUser.address,
         0,
         0,
         0,
         0
-      );
+      )
 
-      await currentUser.provider.waitForTransaction(txHash);
-      const i = count;
-      setCount(count + 1);
-      const result = await getTokenByIndex(contract, i);
-      const results = [...adventurers];
-      results.push(result);
-      setAdventurers(results);
+      await currentUser.provider.waitForTransaction(txHash)
+      const i = count
+      setCount(count + 1)
+      const result = await getTokenByIndex(contract, i)
+      const results = [...adventurers]
+      results.push(result)
+      setAdventurers(results)
     } catch (error) {
-      alert(error.message);
+      alert(error.message)
     }
 
-    setIsMinting(false);
-  };
+    setIsMinting(false)
+  }
 
   useEffect(() => {
-    if (currentUser.isConnected) getAdventurers();
-  }, [currentUser.isConnected]);
+    if (currentUser.isConnected) getAdventurers()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentUser.isConnected])
 
   return (
     <Fragment>
@@ -117,15 +116,15 @@ function Adventurer() {
         {currentUser.isConnected && <Fragment>{adventurers}</Fragment>}
         {(isMinting || isLoading) && (
           <div className='adventurer-loading'>
-            <img></img>
+            <img alt=''></img>
           </div>
         )}
       </div>
-      {count == 0 && !isMinting && !isLoading && (
+      {count === 0 && !isMinting && !isLoading && (
         <p class='hint'>No Adventurer Yet</p>
       )}
     </Fragment>
-  );
+  )
 }
 
-export default Adventurer;
+export default Adventurer
--- src/Quest.js
+++ src/Quest.js
@@ -1,21 +1,21 @@
-import "./App.css";
+import './App.css'
 
-import React, { useState, useContext } from "react";
-import { Contract, hash, Provider, defaultProvider } from "starknet";
-import { useParams } from "react-router-dom";
+import React, { useState, useContext } from 'react'
+import { Contract, defaultProvider } from 'starknet'
+import { useParams } from 'react-router-dom'
 
-import dragonQuestAbi from "./abis/dragonQuest_abi.json";
-import Board from "./components/Board";
-import { UserContext } from "./contexts/user.context";
+import dragonQuestAbi from './abis/dragonQuest_abi.json'
+import Board from './components/Board'
+import { UserContext } from './contexts/user.context'
 
 function Quest() {
-  const [combatting, setCombatting] = useState(false);
-  const [events, setEvents] = useState([]);
-  const [final, setFinal] = useState(0);
+  const [combatting, setCombatting] = useState(false)
+  const [events, setEvents] = useState([])
+  const [final, setFinal] = useState(0)
 
-  const { currentUser } = useContext(UserContext);
+  const { currentUser } = useContext(UserContext)
 
-  const { tokenId } = useParams();
+  const { tokenId } = useParams()
 
   const combat = async () => {
     try {
@@ -23,91 +23,89 @@ function Quest() {
         dragonQuestAbi,
         contractAddress,
         currentUser.provider
-      );
-      const { transaction_hash: txHash } = await contract.combat([tokenId, 0]);
+      )
+      const { transaction_hash: txHash } = await contract.combat([tokenId, 0])
 
-      setCombatting(true);
-      setFinal(0);
+      setCombatting(true)
+      setFinal(0)
 
-      await currentUser.provider.waitForTransaction(txHash);
+      await currentUser.provider.waitForTransaction(txHash)
 
-      setCombatting(false);
-      setFinal(2);
-      getEvents(txHash);
+      setCombatting(false)
+      setFinal(2)
+      getEvents(txHash)
 
       //   getEvents(
       //     "0x5cdaf482a7f729da620c38684a8f8a777c3bd6682471f048c86987cab47128e"
       //   );
     } catch (error) {
-      alert(error.message);
+      alert(error.message)
     }
-  };
+  }
 
   const getEvents = async (txHash) => {
     try {
-      const { events: events } = await defaultProvider.getTransactionReceipt(
-        txHash
-      );
-      setEvents(events);
+      const { events } = await defaultProvider.getTransactionReceipt(txHash)
+      setEvents(events)
     } catch (error) {
-      alert(error.message);
+      alert(error.message)
     }
-  };
+  }
 
   function renderResult() {
-    const results = [];
-    let attack = [];
-    let defend = [];
-    let i = 0;
+    const results = []
+    let attack = []
+    let defend = []
+    let i = 0
     for (i = 0; i < events.length; i++) {
-      if (events[i]["keys"][0] == combatKey) {
-        if (i % 2 == 0) {
-          attack = events[i]["data"];
+      if (events[i]['keys'][0] === combatKey) {
+        if (i % 2 === 0) {
+          attack = events[i]['data']
         } else {
-          defend = events[i]["data"];
+          defend = events[i]['data']
           results.push(
             <Board
               round={Math.floor(i / 2) + 1}
               attack={attack}
               defend={defend}
             />
-          );
+          )
         }
       }
     }
 
-    if (events.length % 2 == 1) {
+    if (events.length % 2 === 1) {
       results.push(
         <Board round={Math.floor(i / 2) + 1} attack={attack} defend={null} />
-      );
+      )
 
-      setFinal(1);
+      setFinal(1)
     }
 
-    return results;
+    return results
   }
 
   const contractAddress =
-    "0x016547928db65c71d9068a719c8f647861c85b945b658406ad5150bc4bc96ce7";
+    '0x016547928db65c71d9068a719c8f647861c85b945b658406ad5150bc4bc96ce7'
 
   const combatKey =
-    "0x250f83f2365231571f91f0649c14039ca8563fe4ecdf12dbe3ce4c731719151";
+    '0x250f83f2365231571f91f0649c14039ca8563fe4ecdf12dbe3ce4c731719151'
 
   return (
-    <main className="App-quest">
+    <main className='App-quest'>
       {currentUser.isConnected && (
         <div>
-          <h1 className="title">Dragon Quest</h1>
+          <h1 className='title'>Dragon Quest</h1>
 
           <div>
-            <p className="description">
+            <p className='description'>
               So you want to be a hero? Well let me have a look at you then! Go
               slaying the dragon to prove you are the kind of fine young souls
               who may just have a chance.
             </p>
 
             {!combatting && (
-              <button className="button" onClick={() => combat()}>
+              <button className='button' onClick={() => combat()}>
                 Go Adventure
               </button>
             )}
@@ -115,24 +113,24 @@ function Quest() {
         </div>
       )}
 
-      <div class="final">
+      <div class='final'>
         {combatting && (
-          <div class="scene">
+          <div class='scene'>
             {/* <img src={process.env.PUBLIC_URL + "dragon.png"}></img> */}
-            <img src="/dragon.png"></img>
+            <img alt='' src='/dragon.png'></img>
           </div>
         )}
 
         {/* final result */}
         {final >= 1 && (
-          <div class="results">
+          <div class='results'>
             <div>{renderResult()}</div>
-            {final == 1 ? (
-              <p class="win">
+            {final === 1 ? (
+              <p class='win'>
                 You have defeated the dragon! Continue on your way...
               </p>
             ) : (
-              <p class="lose">
+              <p class='lose'>
                 You were defeated by the dragon! You are too weak...
               </p>
             )}
@@ -140,7 +138,7 @@ function Quest() {
         )}
       </div>
     </main>
-  );
+  )
 }
 
-export default Quest;
+export default Quest
